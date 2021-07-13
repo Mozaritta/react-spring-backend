dff35e7a58a1762b1a8b56dedb97d2362be7f017
Mozaritta <mozaritta@gmail.com>
2021-07-13 13:00:43 +0100

    Fetch by Id

--- react/src/main/java/com/exp/react/controller/EmployeeController.java
+++ react/src/main/java/com/exp/react/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.exp.react.controller;
 
+import com.exp.react.exception.ResourceNotFoundException;
 import com.exp.react.model.Employee;
 import com.exp.react.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -14,7 +19,8 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-    
+
+    // get all employees
     @GetMapping(value="/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
@@ -25,6 +31,19 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
+    // get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+        return ResponseEntity.ok(employee);
+    }
     
+    // update employee by id
+    @PutMapping("/employee/put")
+    public ResponseEntity<Employee> updateEmployee(Long id, Employee employee){
+        employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No such employee"));
+        return ResponseEntity.ok(employee);
+
+    }
     
 }
