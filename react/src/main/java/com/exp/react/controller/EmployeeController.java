2fef189e541afd632c2cd690a523a57a33191a6b
Mozaritta <mozaritta@gmail.com>
2021-07-15 13:47:19 +0100

    End of project

--- react/src/main/java/com/exp/react/controller/EmployeeController.java
+++ react/src/main/java/com/exp/react/controller/EmployeeController.java
@@ -55,11 +55,11 @@ public class EmployeeController {
     }
     // delete employee rest api
     @DeleteMapping("/employees/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteEmployee(Long id){
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
         employeeRepository.delete(employee);
         Map<String,Boolean> response = new HashMap<>();
-        response.put("deleted", true);
+        response.put("deleted", Boolean.TRUE);
         return ResponseEntity.ok(response);
     }
     
